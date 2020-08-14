c8d2efc3f99060f51fc0305e930aecc51f88bbde
Dan Mikhail <danmikhail+github@gmail.com>
2020-08-13 19:46:46 -0500

    add filter

--- fuelEfficiencyStarter.js
+++ fuelEfficiencyStarter.js
@@ -2,8 +2,11 @@ async function init(){
    
   
 }
-
 async function createTable( idName){
+
+}
+
+async function createChaosTable( idName){
     data = await d3.csv("https://flunky.github.io/cars2017.csv")
     console.log(idName)
     xlogCityMPG = d3.scaleLog(10).domain([10,150]).range([0,200])
@@ -22,7 +25,7 @@ async function createTable( idName){
 
     var gee = d3.selectAll(idName).select("svg").append("g")
     .attr("transform","translate("+margin+","+margin+")")
-    var rectangles = gee.selectAll("circle").data(data).enter().append("circle")
+    var circles = gee.selectAll("circle").data(data).enter().append("circle")
         .attr("cx", function(d,i){return xs(d.AverageCityMPG)})
         .attr("cy", function(d,i){return height - ys(parseInt(d.AverageHighwayMPG) )})
         .attr("r", function(d,i){return (2 + parseInt(d.EngineCylinders) )})
@@ -34,6 +37,41 @@ async function createTable( idName){
     d3.selectAll("svg").append("g")
         .attr("transform","translate("+margin+","+(height+ margin)+")")
         .call(d3.axisBottom(xs).tickValues(tickValues).tickFormat(tickFormat))
+} 
 
+async function createFilterTable( idName){
+    data = await d3.csv("https://flunky.github.io/cars2017.csv")
+    console.log(idName)
+    xlogCityMPG = d3.scaleLog(10).domain([10,150]).range([0,200])
+    yrange = [0,200]
+    ylogHighwayMPG = d3.scaleLog(10).domain([10,150]).range(yrange)
+    margin = 50;
+    height = 200;
+    tickValues = [10,20,50,100]
+    tickFormat = d3.format("~s")
 
+    xs = xlogCityMPG;
+    ys = ylogHighwayMPG;
+    ysInv = d3.scaleLog(10).domain([10,150]).range([yrange[1],yrange[0]]);
+    margin = 50;
+    height = 200;
+
+    var gee = d3.selectAll(idName).select("svg").append("g")
+    .attr("transform","translate("+margin+","+margin+")")
+
+    var circles = gee.selectAll("circle")
+        .data(data.filter(function(d) { return d.EngineCylinders == 6 & d.Fuel == "Gasoline" }))
+        .enter().append("circle")
+        .attr("cx", function(d,i){return xs(d.AverageCityMPG)})
+        .attr("cy", function(d,i){return height - ys(parseInt(d.AverageHighwayMPG) )})
+        .attr("r", function(d,i){return (2 + parseInt(d.EngineCylinders) )})
+    console.log(circles)
+
+    d3.selectAll("svg").append("g")
+        .attr("transform","translate("+margin+","+margin+")")
+        .call(d3.axisLeft(ysInv).tickValues(tickValues).tickFormat(tickFormat))
+
+    d3.selectAll("svg").append("g")
+        .attr("transform","translate("+margin+","+(height+ margin)+")")
+        .call(d3.axisBottom(xs).tickValues(tickValues).tickFormat(tickFormat))
 } 
